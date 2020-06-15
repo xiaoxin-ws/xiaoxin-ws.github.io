4ead11a4855fe86d7157e644430ed2cb144b5f98
xiaoxin-ws <2515061754@qq.com>
2020-06-15 12:04:26 +0800

    blog update

--- js/gaoguai.js
+++
@@ -1,1 +0,0 @@
-var titleTime,OriginTitle=document.title;document.addEventListener("visibilitychange",function(){document.hidden?($('[rel="icon"]').attr("href","/img/TEP.ico"),document.title="╭(°A°`)╮ 你丑你就走 ~",clearTimeout(titleTime)):($('[rel="icon"]').attr("href","/favicon.ico"),document.title="(ฅ>ω<*ฅ) 你帅你美你回来~"+OriginTitle,titleTime=setTimeout(function(){document.title=OriginTitle},2e3))});
